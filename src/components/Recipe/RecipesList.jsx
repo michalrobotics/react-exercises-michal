196ce881925f50f59f5b57ab26864ab166b3e26b
michalrobotics <michalhe07@gmail.com>
2025-05-15 08:55:46 +0300

    removed unused code

--- src/components/Recipe/RecipesList.jsx
+++ src/components/Recipe/RecipesList.jsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect } from "react";
+import { useContext } from "react";
 
 import RecipesContext from "../../store/recipes-context";
 import Recipe from './Recipe';
@@ -6,24 +6,8 @@ import classes from './RecipesList.module.css';
 
 const RecipeList = (props) => {
     const recipesCtx = useContext(RecipesContext);
-    console.log("reevaluating");
 
     const { recipes } = recipesCtx;
-
-    // let recipeListHTML;
-
-    // useEffect(() => {
-    //     recipeListHTML = recipes.map((recipe) => (
-    //         <Recipe
-    //             key={recipe.id}
-    //             name={recipe.name}
-    //             image={recipe.image}
-    //             ingredients={recipe.ingredients}
-    //             steps={recipe.steps}
-    //         />
-    //     ));
-    //     console.log("reloading");
-    // }, [recipes]);
 
     const recipeListHTML = recipes.map((recipe) => (
         <Recipe
